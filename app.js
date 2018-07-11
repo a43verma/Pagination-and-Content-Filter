a73462745e981eda49b04aa3a0a8f2a7e1ec5124
Akash Verma <akash_verma@outlook.com>
2018-07-11 18:23:52 -0400

    search function works with page functions

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-const allStudents = document.querySelectorAll('li');
+let allStudents = document.querySelectorAll('li');
 const studentAmount = allStudents.length;
 const page = document.querySelector('.page');
-const totalPages = Math.ceil(studentAmount/10);
+let totalPages = Math.ceil(studentAmount/10);
 let currentPageNumber = 0;
 
-// function showPage () {
-//   $('.student-item').hide();
-//   for (let i = 0; i < studentAmount; i ++) {
-//     if (i < (currentPageNumber * 10) + 10 && i >= (currentPageNumber * 10)) {
-//       $(allStudents[i]).show();
-//     }
-//   }
-// }
-//
-// function createButtons () {
-//   const newPagesDiv = document.createElement('div');
-//   $(newPagesDiv).addClass('pagination');
-//   page.appendChild(newPagesDiv);
-//   $('.pagination').append("<button id='link1' class='active'>1</button>")
-//   for (let i = 2; i <= totalPages; i ++) {
-//     $('.pagination').append("<button id='link" + i + "'>" + i + "</button>");
-//   }
-// }
-//
-// showPage();
-// createButtons();
-//
-// $('button').on('click', function(event){
-//   let $target = $(event.target);
-//   $('button').removeClass('active');
-//   $target.addClass('active');
-//   currentPageNumber = $target.text() - 1;
-//   showPage();
-// })
+function showPage () {
+  $('.student-item').hide();
+  for (let i = 0; i < studentAmount; i ++) {
+    if (i < (currentPageNumber * 10) + 10 && i >= (currentPageNumber * 10)) {
+      $(allStudents[i]).show();
+    }
+  }
+}
+
+function createButtons () {
+  const newPagesDiv = document.createElement('div');
+  $(newPagesDiv).addClass('pagination');
+  page.appendChild(newPagesDiv);
+  $('.pagination').append("<button id='link1' class='active'>1</button>")
+  for (let i = 2; i <= totalPages; i ++) {
+    $('.pagination').append("<button id='link" + i + "'>" + i + "</button>");
+  }
+}
+
+showPage();
+createButtons();
+
+$('button').on('click', function(event){
+  let $target = $(event.target);
+  $('button').removeClass('active');
+  $target.addClass('active');
+  currentPageNumber = $target.text() - 1;
+  showPage();
+})
 
 $('.page-header').append('<input type="text" class="student-search" id="myInput" onkeyup="searchNames()" placeholder="Search by names...">');
 
 function searchNames () {
   const input = $('#myInput').val().toUpperCase();
-  console.log(input);
+  $('button').hide();
   $('.student-details h3').each(function (index) {
     let name = $(this).text().toUpperCase();
     let student = $(this).closest('.student-item');
@@ -49,9 +49,3 @@ function searchNames () {
     }
   })
 }
-
-// Search function
-// create search button
-// receive text from search button and stop it from reloading the page
-// loop through each of the names in the list
-//

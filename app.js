e8298e256be4a685e740e8df91c6afe05f00d6c9
Akash Verma <akash_verma@outlook.com>
2018-07-11 18:15:28 -0400

    Search Function works independently

--- app.js
+++ app.js
@@ -1,36 +1,57 @@
-const students = document.querySelectorAll('li');
-const studentAmount = students.length;
+const allStudents = document.querySelectorAll('li');
+const studentAmount = allStudents.length;
+const page = document.querySelector('.page');
+const totalPages = Math.ceil(studentAmount/10);
 let currentPageNumber = 0;
-console.log(currentPageNumber);
 
-function showPage () {
-  $('.student-item').hide();
-  for (let i = 0; i < studentAmount; i ++) {
-    if (i < (currentPageNumber * 10) + 10 && i >= (currentPageNumber *10)) {
-      $(students[i]).show();
+// function showPage () {
+//   $('.student-item').hide();
+//   for (let i = 0; i < studentAmount; i ++) {
+//     if (i < (currentPageNumber * 10) + 10 && i >= (currentPageNumber * 10)) {
+//       $(allStudents[i]).show();
+//     }
+//   }
+// }
+//
+// function createButtons () {
+//   const newPagesDiv = document.createElement('div');
+//   $(newPagesDiv).addClass('pagination');
+//   page.appendChild(newPagesDiv);
+//   $('.pagination').append("<button id='link1' class='active'>1</button>")
+//   for (let i = 2; i <= totalPages; i ++) {
+//     $('.pagination').append("<button id='link" + i + "'>" + i + "</button>");
+//   }
+// }
+//
+// showPage();
+// createButtons();
+//
+// $('button').on('click', function(event){
+//   let $target = $(event.target);
+//   $('button').removeClass('active');
+//   $target.addClass('active');
+//   currentPageNumber = $target.text() - 1;
+//   showPage();
+// })
+
+$('.page-header').append('<input type="text" class="student-search" id="myInput" onkeyup="searchNames()" placeholder="Search by names...">');
+
+function searchNames () {
+  const input = $('#myInput').val().toUpperCase();
+  console.log(input);
+  $('.student-details h3').each(function (index) {
+    let name = $(this).text().toUpperCase();
+    let student = $(this).closest('.student-item');
+    if (name.indexOf(input) > -1) {
+      student.show();
+    } else {
+      student.hide();
     }
-  }
+  })
 }
 
-function createButtons () {
-  const totalPages = Math.floor(studentAmount/10);
-  const page = document.querySelector('.page');
-  const newPagesDiv = document.createElement('div');
-  $(newPagesDiv).addClass('pagination');
-  page.appendChild(newPagesDiv);
-  $('.pagination').append("<button id='link1' class='active'>1</button>")
-  for (let i = 2; i <= totalPages; i ++) {
-    $('.pagination').append("<button id='link" + i + "'>" + i + "</button>");
-  }
-}
-
-showPage();
-createButtons();
-
-$('button').on('click', function(event){
-  let $target = $(event.target);
-  $('button').removeClass('active');
-  $target.addClass('active');
-  currentPageNumber = $target.text();
-  showPage();
-})
+// Search function
+// create search button
+// receive text from search button and stop it from reloading the page
+// loop through each of the names in the list
+//

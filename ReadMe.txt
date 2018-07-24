1610e07ed5aa3a01d5861e9154792336fb362d6f
Akash Verma <akash_verma@outlook.com>
2018-07-24 19:55:51 -0400

    final

---
+++ ReadMe.txt
@@ -0,0 +1,12 @@
+Pagination and Content Filter:
+
+I built this project for the completion of Unit 2 of my Treehouse Techdegree assignments.
+It takes the list items from the index.html and only lets 10 students display at a time, while creating links
+to navigate throughout the rest of the students. The program also creates a search function.
+
+This project was built using a combination of standard JavaScript and JQuery.
+
+How it works:
+The program divides the total number of student items into sections of 10. Another function creates links to
+modify the function to engage a different section of 10. The search function, gives the pagination function
+a new class to use and paginate.
--- app.js
+++ app.js
@@ -1,62 +1,90 @@
-let allStudents = document.querySelectorAll('li');
-const studentAmount = allStudents.length;
-const page = document.querySelector('.page');
+const newUl = document.createElement('ul');
+let totalPages = Math.ceil($('.student-item').length/10);
+allStudents = $('.student-item').length;
+$listOfStudents = $('.student-item');
 
-let currentPageNumber = 0;
-
-function showPage () {
-  $('.student-item').hide();
-  for (let i = 0; i < studentAmount; i ++) {
-    if (i < (currentPageNumber * 10) + 10 && i >= (currentPageNumber * 10)) {
-      $(allStudents[i]).show();
-    }
-  }
-}
-
-function createButtons () {
-  const newPagesDiv = document.createElement('div');
-  $(newPagesDiv).addClass('pagination');
-  page.appendChild(newPagesDiv);
-  $('.pagination').append("<button id='link1' class='active'>1</button>")
-  for (let i = 2; i <= totalPages; i ++) {
-    $('.pagination').append("<button id='link" + i + "'>" + i + "</button>");
-  }
-}
-
-function removeButtons () {
-
-}
-
-showPage(allStudents);
-createButtons();
-
-$('button').on('click', function(event){
-  let $target = $(event.target);
-  $('button').removeClass('active');
-  $target.addClass('active');
-  currentPageNumber = $target.text() - 1;
-  showPage();
-})
-
+// Attaches a search bar
 $('.page-header').append('<input type="text" class="student-search" id="myInput" onkeyup="searchNames()" placeholder="Search by names...">');
 
-function searchNames () {
-  const input = $('#myInput').val().toUpperCase();
-  $('button').hide();
-  let name;
-  let newStudentList = [];
-  for (let i = 0; i < studentAmount; i ++) {
-    name = allStudents[i].querySelector('h3').innerHTML.toUpperCase();
-    if (name.indexOf(input) > -1) {
-      console.log(allStudents[i]);
-      newStudentList.push(allStudents[i]);
+// Display students based on page numbers
+function showPage (pageNumber, numberOfStudents, listOfStudents) {
+  $('.student-item').hide();
+  for (let i = 0; i < numberOfStudents; i ++) {
+    if (i < ((pageNumber - 1) * 10) + 10 && i >= ((pageNumber - 1) * 10)) {
+      $(listOfStudents).eq(i).show();
     }
-    if (newStudentList.length = 0) {
-      $('.student-list').text("<h1>No students were found</h1>");
-    }
-    console.log(newStudentList)
-    allStudents = newStudentList;
-    showPage();
-
-    allStudents = document.querySelectorAll('li');
   }
+}
+
+// Creates the initial section for the links to go
+function createPagination () {
+  $(newUl).addClass('pagination');
+  $('.page').append(newUl);
+}
+
+// Creates a link with a page number
+function createLink (i) {
+  let newLi = document.createElement('li');
+  let newLink = document.createElement('a');
+  $(newLi).addClass('links');
+  $(newLink).text('' + i + '');
+  $(newLi).append(newLink);
+  $('.pagination').append(newLi);
+}
+
+// Repeats the createLink function depending on the number of pages
+function allLinks (amount) {
+  for (let i = 1; i <= amount; i ++) {
+    createLink(i);
+  }
+}
+
+// Removes the links for pagination
+function removeLinks () {
+  $('.links').remove();
+}
+
+// Search function that goes through the name and returns results
+function searchNames () {
+  let $input = $('#myInput').val().toUpperCase();
+  // Removes the previous links
+  removeLinks();
+
+  // Removes the error message
+  $('.error').remove();
+
+  // Hides all the students on the page
+  $('.student-item').hide();
+
+  // goes through each student item and determines if the input from the search is found
+  $('.student-item h3').each(function (index) {
+    if ($(this).text().toUpperCase().indexOf($input) > -1 ) {
+      // this class is used to create the new pagination with only found results
+      $(this).closest('.student-item').addClass('found');
+    } else {
+      // removes the found class from every other list item
+      $(this).closest('.student-item').removeClass('found');
+    }
+  });
+
+  // gives an error message if no results are found
+  if ($('.found').length === 0) {
+    $('.page').append('<h1 class="error">Sorry, your search has returned 0 results</h1>');
+  }
+
+  // re-does the pagination on the page using the new criteria
+  allLinks(Math.ceil($('.found').length/10));
+  showPage(1, $('.found').length, $('.found'));
+}
+
+// calls on the functions to get the initial page setup
+createPagination();
+allLinks(totalPages);
+showPage(1, allStudents, $listOfStudents);
+
+// Gives the links the ability to go through the student list
+$('.pagination li a').on('click', function(event){
+  let $target = $(event.target);
+  let pageNumber = $target.text();
+  showPage(pageNumber, allStudents, $listOfStudents);
+})
